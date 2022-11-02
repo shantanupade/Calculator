fdda71e12766b28eb450efcf4608dc07cc4e6f96
shantanupade <shantanupade1999@gmail.com>
2022-11-02 17:40:20 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,97 @@
+let equation = document.querySelector("#equation");
+let finalOutput = document.querySelector("#finalOutput");
+let allButtons = document.querySelectorAll("button");
+
+for (button of allButtons){
+    button.addEventListener("click",(e)=>{
+        btnText = e.target.innerText;
+        finalOutput.value = btnText; 
+        console.log(btnText)
+        // if(){
+
+        // }
+    })
+}
+
+
+
+
+
+
+
+
+// document.querySelector(".leftBracket").addEventListener("click",()=>{
+
+// })
+
+// document.querySelector(".leftBracket").addEventListener("click",()=>{
+    
+// })
+
+// document.querySelector(".leftBracket").addEventListener("click",()=>{
+    
+// })
+
+// document.querySelector(".leftBracket").addEventListener("click",()=>{
+    
+// })
+
+// document.querySelector(".leftBracket").addEventListener("click",()=>{
+    
+// })
+
+// document.querySelector(".leftBracket").addEventListener("click",()=>{
+    
+// })
+
+// document.querySelector(".leftBracket").addEventListener("click",()=>{
+    
+// })
+
+// document.querySelector(".leftBracket").addEventListener("click",()=>{
+    
+// })
+
+// document.querySelector(".leftBracket").addEventListener("click",()=>{
+    
+// })
+
+// document.querySelector(".leftBracket").addEventListener("click",()=>{
+    
+// })
+
+// document.querySelector(".leftBracket").addEventListener("click",()=>{
+    
+// })
+
+// document.querySelector(".leftBracket").addEventListener("click",()=>{
+    
+// })
+
+// document.querySelector(".leftBracket").addEventListener("click",()=>{
+    
+// })
+
+// document.querySelector(".leftBracket").addEventListener("click",()=>{
+    
+// })
+
+// document.querySelector(".leftBracket").addEventListener("click",()=>{
+    
+// })
+
+// document.querySelector(".leftBracket").addEventListener("click",()=>{
+    
+// })
+
+// document.querySelector(".leftBracket").addEventListener("click",()=>{
+    
+// })
+
+// document.querySelector(".leftBracket").addEventListener("click",()=>{
+    
+// })
+
+// document.querySelector(".leftBracket").addEventListener("click",()=>{
+    
+// })
